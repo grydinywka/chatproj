9aa6a93c98f01b1823a9e44e3322299f89788703
Sergiy Ignatenko <grydinywka@gmail.com>
2016-08-01 02:30:23 +0300

    fix bugs

---
+++ static/js/chat.js
@@ -0,0 +1,79 @@
+function scrollToBottom() {
+    var height = $('body').prop('scrollHeight');
+
+    $(window).scrollTop(height);
+}
+
+function appendNotice() {
+    $("#add_notice").click(function(event) {
+        var form = $('form#new-msg-form');
+
+        $.ajax({
+            'url': form.attr('action'),
+            'type': 'POST',
+            'async': true,
+            'dataType': 'json',
+            'data': {
+                'add_notice': true,
+                'content': $('input#content').val(),
+                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+            },
+            'beforeSend': function() {
+                event.preventDefault();
+            },
+            'success': function(data, status, xhr) {
+                var notice_error = $('#notice-error');
+
+                if ( 'content_error' in data ) {
+                    notice_error.text(data.content_error);
+                    notice_error.parent().addClass('has-error');
+                } else {
+                    var list_message = $('#msg-list ul');
+
+                    notice_error.text('');
+                    notice_error.parent().removeClass('has-error');
+                    $('#content').val('');
+
+                    list_message.append(
+                        '<li class="list-group-item">(' +
+                             data.created + ') <strong>' + data.username +
+                            '</strong><p>' + data.content + '</p>' +
+                        '</li>'
+                    );
+//                    getNotices();
+                }
+                scrollToBottom();
+            },
+            'error': function() {
+                alert('Error on server');
+            }
+        });
+    });
+}
+
+function getNotices() {
+    $.get('/chat/', function(html) {
+        var html = $(html);
+        var messages_from_db = html.find('div#msg-list ul');
+        var messages_from_chat = $('div#msg-list ul');
+
+        if ( messages_from_db.children().length > messages_from_chat.children().length ) {
+//            alert(messages_from_db.children().length);
+//            alert(messages_from_chat.children().length);
+            messages_from_chat.html(messages_from_db.children());
+            if ( $('body').prop('scrollHeight') - $(window).scrollTop() < 2000 ) {
+                scrollToBottom();
+            }
+        }
+//        $('div#msg-list ul')
+    });
+
+//    scrollToBottom();
+}
+
+$(document).ready(function() {
+    scrollToBottom();
+    appendNotice();
+    setInterval(getNotices, 500);
+
+});
--- static/js/main.js
+++ static/js/main.js
@@ -1,81 +1,15 @@
 // js file
+function checkForChanges() {
+    var $element = $("#nav");
 
-function appendNotice() {
-    $("#add_notice").click(function(event) {
-        var form = $('form#new-msg-form');
-
-        $.ajax({
-            'url': form.attr('action'),
-            'type': 'POST',
-            'async': true,
-            'dataType': 'json',
-            'data': {
-                'add_notice': true,
-                'content': $('input#content').val(),
-                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
-            },
-            'beforeSend': function() {
-                event.preventDefault();
-            },
-            'success': function(data, status, xhr) {
-                var notice_error = $('#notice-error');
-
-                if ( 'content_error' in data ) {
-                    notice_error.text(data.content_error);
-                    notice_error.parent().addClass('has-error');
-                } else {
-                    var list_message = $('#msg-list ul');
-
-                    notice_error.text('');
-                    notice_error.parent().removeClass('has-error');
-                    $('#content').val('');
-
-                    list_message.append(
-                        '<li class="list-group-item">(' +
-                             data.created + ') <strong>' + data.username +
-                            '</strong><p>' + data.content + '</p>' +
-                        '</li>'
-                    );
-                    scrollToBottom();
-                }
-            },
-            'error': function() {
-                alert('Error on server');
-            }
-        });
-    });
-}
-
-function scrollToBottom() {
-    var height = $('body').prop('scrollHeight');
-
-    $(window).scrollTop(height);
-}
-
-function getNotices() {
-    $.get('/chat/', function(html) {
-        var html = $(html);
-        var messages = html.find('div#msg-list ul');
-
-        $('div#msg-list ul').html(messages);
-    });
-
-//    scrollToBottom();
-}
-
-function refreshChat() {
-    getNotices();
-//    alert($('body').prop('scrollHeight'));
-//    alert($(window).scrollTop());
-    if ( $('body').prop('scrollHeight') - $(window).scrollTop() < 2000 ) {
-
-        scrollToBottom();
+    if ($element.css('height') != $('#messages').css('margin-top')) {
+        $('#messages').css("margin-top", $element.css('height'));
     }
 
+    setTimeout(checkForChanges, 500);
 }
 
 $(document).ready(function() {
-    scrollToBottom();
-    appendNotice();
-    setInterval(getNotices, 2500);
+    checkForChanges();
+
 });
