c8edee4eedc48d0b6fc674db79afda0b2c4bd56a
Sergiy Ignatenko <grydinywka@gmail.com>
2016-08-01 08:10:57 +0300

    fix margin top of dj-messages

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ function checkForChanges() {
     var $element = $("#nav");
 
     if ($element.css('height') != $('#messages').css('margin-top')) {
-        $('#messages').css("margin-top", $element.css('height'));
+        $('#messages').css("margin-top", $element.height() + 10);
     }
 
     setTimeout(checkForChanges, 500);
