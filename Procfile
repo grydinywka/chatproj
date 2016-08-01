e7e704da5bae9087c930528820b06e19773cc14e
Sergiy Ignatenko <grydinywka@gmail.com>
2016-08-01 07:20:03 +0300

    prepare to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chatproj.wsgi:application
--- chatproj/settings.py
+++ chatproj/settings.py
@@ -90,6 +90,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
 
 
 # Password validation
@@ -133,6 +136,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Simplified static file serving. # https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+ # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+PORTAL_URL = 'http://localhost:8000'
 
 REGISTRATION_OPEN = True
 
